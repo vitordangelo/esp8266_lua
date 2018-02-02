e66db997d88b6e288b3822094511cb9bb25d937c
vitor <metroid.p2p@gmail.com>
2018-02-01 22:56:09 -0200

    gpa

--- gps.lua
+++ gps.lua
@@ -1,4 +1,4 @@
-uart.setup(0, 9600, 8, uart.PARITY_NONE, uart.STOPBITS_1, 1)
+-- uart.setup(0, 9600, 8, uart.PARITY_NONE, uart.STOPBITS_1, 1)
 uart.on("data","\n",function(data)
   if (string.sub(data, 2, 6) == "GPRMC") then
     print(data)
@@ -8,15 +8,22 @@ uart.on("data","\n",function(data)
     local segundos = string.sub(data, 12, 13)
     -- print(hora .. ":" .. minutos .. ":" .. segundos)
 
-    local latGP = string.sub(data, 21, 22)
+    local latGP = tonumber(string.sub(data, 21, 22))
     local latGP1 = string.sub(data, 23, 29)
+    print(latGP .. "|" .. latGP1)
 
-    local part1 = string.sub(latGP1, 1, 2)
-    local part2 = string.sub(latGP1, 3, 6)
-    local part3 = part1 .. part2
-    
-    print(part1)
-    print(part2)
+    print(type(latGP1))
+    print(tonumber(latGP1))
+    print(tonumber(latGP1) / 1000)
+
+    -- local latGP1Part1 = string.sub(latGP1, 1, 2)
+    -- local latGP1Part2 = string.sub(latGP1, 4, 7)
+    -- local latGPAdjusted = latGP1Part1 .. latGP1Part2
+
+    -- print(tonumber(latGPAdjusted)/1000)
+
+    -- local divided = tonumber(latGPAdjusted / 60)
+    -- print(latGP + divided)
 
   end
 end, 0)
@@ -24,3 +31,5 @@ end, 0)
 -- receive from uart:	$GPRMC,132039.000,A,2215.3513,S,04541.7069,W,0.34,196.16,310118,,,A*63
 -- 3731.9404  ----> 37 + 31.9404/60 = 37.53234 degrees
 -- 10601.6986 ---> 106+1.6986/60 = 106.02831 degrees
+
+-- 2215.662 ----> 22 + 15.662/60 = 22.2610
