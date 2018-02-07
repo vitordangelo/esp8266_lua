d71bf88accf865e782c6e74ac8ef8e9c99589b2f
Vitor <metroid.p2p@gmail.com>
2018-02-07 16:21:10 -0200

    uploader

---
+++ nodemcu-uploader/app.lua
@@ -0,0 +1,102 @@
+print("App...")
+print(statusModuleTopic)
+print(hash  )
+
+m = mqtt.Client(hash, 5)
+m:lwt(statusModuleTopic, "0", 0, 0)
+
+m:on("connect",function(m) 
+  print("Connecting to broker... ") 
+  m:subscribe({[statusArmDisarmCentralAlarmTopic]=0, [timerRelayTimedTopic]=0}, 
+    0, function(m) print("sub done") 
+  end)
+  
+  m:publish(statusModuleTopic, "1", 0, 1)
+  m:publish(statusRelayFixedTopic, "0", 0, 1)
+  m:publish(statusRelayTimedTopic, "0", 0, 1)
+  m:publish(statusTriggerTopic, "0", 0, 1)
+  m:publish(statusCentralAlarmTopic, "0", 0, 1)
+end )
+
+m:on("offline", function(conn)
+  print("Disconnect to broker...")
+  print("Will restart...")
+  node.restart()
+end)
+
+m:connect("v2alarm.ddns.net",1883,0,0)
+
+m:on("message", function(client, topic, data) 
+  if data ~= nil then
+    if (topic == statusArmDisarmCentralAlarmTopic) then
+      print(data)
+      if data == "0" then
+        disarmCentralAlarm()
+      end
+      if data == "1" then
+        armCentralAlarm()
+      end
+      if data == "3" then
+        relayOutputFixedOn()
+      end
+      if data == "4" then
+        relayOutputFixedOff()
+      end
+    end
+
+    if (topic == timerRelayTimedTopic) then
+      print("Relay Timed On")
+      gpio.write(relayTimedOutput, 1)
+      m:publish(statusRelayTimedTopic, "1", 0, 1)
+      tmr.alarm(idTimerRelayTimer, data, 0, function()
+        gpio.write(relayTimedOutput, 0)
+        m:publish(statusRelayTimedTopic, "0", 0, 0)
+        print("Relay Timed Off")
+      end)
+    end
+  end
+end)
+
+relayOutputFixedOn = function 
+  ()
+  gpio.write(relayOutput, 1)
+  m:publish(statusRelayFixedTopic, "1", 0, 1)
+  print("relayOutputFixedOn")
+end
+
+relayOutputFixedOff = function 
+  ()
+  gpio.write(relayOutput, 0)
+  m:publish(statusRelayFixedTopic, "0", 0, 1)
+  print("relayOutputFixedOff")
+end
+
+armCentralAlarm = function 
+  ()
+  gpio.write(armDisarmOutput, 1)
+  print("armCentralAlarm")
+end
+
+disarmCentralAlarm = function 
+  ()
+  gpio.write(armDisarmOutput, 0)
+  print("disarmCentralAlarm")
+end
+
+tmr.alarm(idTimerCentralAlarmTrigger, 1000, 1, function()
+  alarmTrigger = gpio.read(centralAlarmTrigger)
+  if (alarmTrigger ~= alarmTriggerChanged) then
+    m:publish(statusTriggerTopic, alarmTrigger, 0, 1)
+    print("alarmTrigger: " .. alarmTrigger)
+    alarmTriggerChanged = alarmTrigger
+  end
+end)
+
+tmr.alarm(idTimerCentralAlarmState, 1000, 1, function()
+  centralAlarmState = gpio.read(centralAlarmStateInput)
+  if (centralAlarmState ~= centralAlarmStateChanged) then
+    m:publish(statusCentralAlarmTopic, centralAlarmState, 0, 1)
+    print("centralAlarmState " .. centralAlarmState)
+    centralAlarmStateChanged = centralAlarmState
+  end
+end)
---
+++ nodemcu-uploader/nodemcu-uploader.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2015-2016 Peter Magnusson <peter@birchroad.net>
+#pylint: disable=C0103
+"""makes it easier to run nodemcu-uploader from command line"""
+
+from nodemcu_uploader import main
+
+
+if __name__ == '__main__':
+    main.main_func()
